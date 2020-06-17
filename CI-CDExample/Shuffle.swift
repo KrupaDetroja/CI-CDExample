d366a99317a5b9cb84922dfde3acafd3e7b38ebf
Mayur Parmar <mrparmar2997@gmail.com>
2020-06-17 15:05:51 +0530

    - Build 2/

--- CI-CDExample/Shuffle.swift
+++ CI-CDExample/Shuffle.swift
@@ -23,7 +23,7 @@ extension MutableCollection {
 }
 
 extension Sequence {
-  /// Returns an array with the contents of this sequence, shuffled.
+  /// Returns an array with the contents of this sequence, shuffled
   func shuffled() -> [Element] {
     var result = Array(self)
     result.shuffle()
